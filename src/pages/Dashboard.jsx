d1246ccd031e4f72e4c04e82329f8498922fc7f7
oumaimameddah <91436153+oumaimameddah@users.noreply.github.com>
2023-02-14 16:57:56 +0100

    Si l'api n'est pas dispo ne pas cracher la page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -27,6 +27,7 @@ function Dashboard() {
     const [getUserAverageSessionById, setgetUserAverageSessionById] = useState({});
     const [getUserPerformanceById, setgetUserPerformanceById] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const [isServiceNotAvailable, setIsServiceNotAvailable] = useState(false);
 
     useEffect(() => {
         const fetch = async (id, switcher) => {
@@ -35,11 +36,18 @@ function Dashboard() {
             const AVERAGE_SESSIONS = await getAverageSessions(id, switcher);
             const PERFORMANCE = await getPerformance(id, switcher);
 
-            setgetUserById(USER);
-            setgetUserActivityById(ACTIVITY);
-            setgetUserAverageSessionById(AVERAGE_SESSIONS);
-            setgetUserPerformanceById(PERFORMANCE);
-            setIsLoading(false);
+            if (!USER || !ACTIVITY || !AVERAGE_SESSIONS || !PERFORMANCE) {
+                setIsServiceNotAvailable(true);
+                console.log("NOT AVAILABLE")
+            } else {
+                setgetUserById(USER);
+                setgetUserActivityById(ACTIVITY);
+                setgetUserAverageSessionById(AVERAGE_SESSIONS);
+                setgetUserPerformanceById(PERFORMANCE);
+                setIsLoading(false);
+            }
+
+
         };
         fetch(id, switcher);
     }, [id, switcher]);
@@ -57,57 +65,64 @@ function Dashboard() {
 
     return (
         <div className="dashboard-section dashboard">
-            { isLoading && token === id ?
-                (
-                    <p>Loading data... Tik Tak Tok...</p>
-                ) : (
-                    <>
-                        <div className="dashboardHeader">
-                            <h1 className="dashboardHeader__title">Bonjour<span
-                                className="dashboardHeader__name">{" " + USER_CLASS.firstName}</span></h1>
-                            <p className="dashboardHeader__text">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-                        </div>
-                        <div className="dashboard__charts">
-                            <div className="dashboard__charts-left">
-                                <Activity userActivityData={getUserActivityById} />
-                                <AverageSession averageSessionsData={getUserAverageSessionById} />
-                                <Performance performanceData={getUserPerformanceById} />
-                                <Score scoreData={USER_CLASS.arrayOfPercentScore} />
-                            </div>
-                            <div className="dashboard__charts-right">
-                                <Kpi
-                                    image={energy}
-                                    value={USER_CLASS.calorie}
-                                    title="Calories"
-                                    unity="kCal"
-                                    color="red"
-                                />
-                                <Kpi
-                                    image={chicken}
-                                    value={USER_CLASS.protein}
-                                    title="Proteines"
-                                    unity="g"
-                                    color="blue"
-                                />
-                                <Kpi
-                                    image={apple}
-                                    value={USER_CLASS.carbohydrate}
-                                    title="Glucides"
-                                    unity="g"
-                                    color="yellow"
-                                />
-                                <Kpi
-                                    image={cheeseburger}
-                                    value={USER_CLASS.lipid}
-                                    title="Lipides"
-                                    unity="g"
-                                    color="pink"
-                                />
-                            </div>
-                        </div>
-                    </>
+            {
+                isServiceNotAvailable ? (
+                    <p>Service Not Available 😭⛔️🆘!!!</p>
+                ):(
+                     isLoading && token === id ?
+                        (
+                            <p>Loading data... Tik Tak Tok...⏳💪🏻</p>
+                        ) : (
+                            <>
+                                <div className="dashboardHeader">
+                                    <h1 className="dashboardHeader__title">Bonjour<span
+                                        className="dashboardHeader__name">{" " + USER_CLASS.firstName}</span></h1>
+                                    <p className="dashboardHeader__text">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+                                </div>
+                                <div className="dashboard__charts">
+                                    <div className="dashboard__charts-left">
+                                        <Activity userActivityData={getUserActivityById} />
+                                        <AverageSession averageSessionsData={getUserAverageSessionById} />
+                                        <Performance performanceData={getUserPerformanceById} />
+                                        <Score scoreData={USER_CLASS.arrayOfPercentScore} />
+                                    </div>
+                                    <div className="dashboard__charts-right">
+                                        <Kpi
+                                            image={energy}
+                                            value={USER_CLASS.calorie}
+                                            title="Calories"
+                                            unity="kCal"
+                                            color="red"
+                                        />
+                                        <Kpi
+                                            image={chicken}
+                                            value={USER_CLASS.protein}
+                                            title="Proteines"
+                                            unity="g"
+                                            color="blue"
+                                        />
+                                        <Kpi
+                                            image={apple}
+                                            value={USER_CLASS.carbohydrate}
+                                            title="Glucides"
+                                            unity="g"
+                                            color="yellow"
+                                        />
+                                        <Kpi
+                                            image={cheeseburger}
+                                            value={USER_CLASS.lipid}
+                                            title="Lipides"
+                                            unity="g"
+                                            color="pink"
+                                        />
+                                    </div>
+                                </div>
+                            </>
+                        )
+
                 )
             }
+
         </div>
     )
 }
